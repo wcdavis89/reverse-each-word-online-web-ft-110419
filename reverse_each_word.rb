03431e2bc648d7b9f821d2e04f3b5f96cb304ec9
Whittney Davis <wcdavis89@yahoo.com>
2019-11-12 22:28:03 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,3 +6,12 @@ new_string.each do |word|
   end
   reversed.join(" ")
 end
+
+def reverse_each_word(string)
+reversed=[]
+new_string=string.split
+new_string.each do |word|
+   reversed.push(word.reverse)
+  end
+ puts reversed.join(" ")
+end
